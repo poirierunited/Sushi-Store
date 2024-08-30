ea06fba3f942dc9b5654870c316efd380786489d
Alejandro Poirier <alejandropoirier@MacBook-Air-de-Alejandro.local>
2024-08-30 06:47:40 -0400

    fix: handle errors correctly

--- api/routes/User.js
+++ api/routes/User.js
@@ -18,8 +18,39 @@ userRoute.post(
         createdAt: user.createdAt,
       });
     } else {
-      res.status(401);
-      throw new Error("Invalid email or password");
+      res.status(401).json({ message: "Invalid email or password" });
+      return;
+    }
+  })
+);
+
+//register route
+userRoute.post(
+  "/",
+  AsyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ message: "Email already registered" });
+      return;
+    } else {
+      const user = await User.create({
+        name: name,
+        email: email,
+        password: password,
+      });
+
+      if (user) {
+        res.status(201).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          createdAt: user.createdAt,
+        });
+      } else {
+        res.status(400).json({ message: "Invalid User Data" });
+      }
     }
   })
 );

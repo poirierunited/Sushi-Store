c825f04d0ebcad3c7745b62ff78e4031e8b2bcf1
Alejandro Poirier <alejandropoirier@MacBook-Air-de-Alejandro.local>
2024-09-06 18:55:32 -0400

    Translate text to spanish

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -19,7 +19,7 @@ function ProductDetail() {
     <>
       <Layout>
         {loading ? (
-          <h1>loading</h1>
+          <h1>cargando...</h1>
         ) : error ? (
           <h1>{error}</h1>
         ) : (
@@ -34,7 +34,7 @@ function ProductDetail() {
                   />
                   <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                     <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                      Price: ${product.price}
+                      Precio: {product.price}$
                     </h2>
                     <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
                       {product.name}
@@ -97,10 +97,11 @@ function ProductDetail() {
                           <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                         </svg>
                         <span className="text-gray-600 ml-3">
-                          {product.numReview} Reviews
+                          {product.numReview} Opiniones
                         </span>
                       </span>
-                      <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
+                      {/* TODO!: DEFINIR SI AGREGAR LOGOS DE REDES SOCIALES */}
+                      {/* <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
                         <a className="text-gray-500">
                           <svg
                             fill="currentColor"
@@ -137,17 +138,17 @@ function ProductDetail() {
                             <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                           </svg>
                         </a>
-                      </span>
+                      </span> */}
                     </div>
                     <p className="leading-relaxed">{product.description}</p>
                     <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                      <div className="flex">
+                      {/* <div className="flex">
                         <span className="mr-3">Color</span>
                         <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
                         <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
                         <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                      </div>
-                      <div className="flex ml-6 items-center">
+                      </div> */}
+                      {/* <div className="flex ml-6 items-center">
                         <span className="mr-3">Size</span>
                         <div className="relative">
                           <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
@@ -170,7 +171,7 @@ function ProductDetail() {
                             </svg>
                           </span>
                         </div>
-                      </div>
+                      </div> */}
                     </div>
                     <div className="flex">
                       <span className="title-font font-medium text-2xl text-gray-900">

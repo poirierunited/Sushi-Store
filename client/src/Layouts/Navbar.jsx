c761ba7e44e5459e7cb7b365a2e4a51d0f2d73a7
Alejandro Poirier <alejandropoirier@MacBook-Air-de-Alejandro.local>
2024-09-10 20:05:36 -0300

    Translate text to spanish

--- client/src/Layouts/Navbar.jsx
+++ client/src/Layouts/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             {/* TODO!: AGREGAR LOGO */}
-            <img src="#" className="h-8" alt="Flowbite Logo" />
+            <img src="#" className="h-8" alt="Fukase Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Fukusuke Sushi
             </span>
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -25,13 +25,13 @@ export default function Register() {
             htmlFor="email"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Your email
+            Email
           </label>
           <input
             type="email"
             id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
+            placeholder="nombre@email.com"
             required
           />
         </div>
@@ -40,7 +40,7 @@ export default function Register() {
             htmlFor="password"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Your password
+            Contraseña
           </label>
           <input
             type="password"
@@ -63,14 +63,14 @@ export default function Register() {
             htmlFor="remember"
             className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
-            Remember me
+            Recordarme al iniciar sesión
           </label>
         </div>
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Submit
+          Aceptar
         </button>
       </form>
     </Layout>

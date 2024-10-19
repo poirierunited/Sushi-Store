80da08c600822e0ff3365d4d91395ce58457bc55
Zirnit <111541556+Zirnit@users.noreply.github.com>
2024-10-19 00:25:20 -0300

    Refactor alt text in Products component

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -31,7 +31,7 @@ const Products = () => {
                             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                               <img
                                 src={product.image}
-                                alt="Front of men&#039;s Basic Tee in black."
+                                alt={product.name}
                                 className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                               />
                             </div>

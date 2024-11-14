c3d005d754d0cbb55f8941dd2126f0c62d9d7b10
Alejandro Poirier <alejandropoirier@MacBook-Air-de-Alejandro.local>
2024-11-14 19:35:47 -0300

    Modify report data && check for authorization

--- api/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-// middleware/Admin.js
-module.exports = (req, res, next) => {
-    if (req.user && req.user.isAdmin) {
-      next();
-    } else {
-      res.status(401).json({ message: "Not authorized as an admin" });
-    }
-  };
--- api/models/User.js
+++ api/models/User.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema(
     phoneNumber: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );

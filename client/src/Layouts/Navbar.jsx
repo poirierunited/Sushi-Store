3efefae1b9fd1dfac67e0736b5a9f979cdda7d02
Alejandro Poirier <alejandropoirier@MacBook-Air-de-Alejandro.local>
2024-11-17 19:37:24 -0300

    Make design responsive

--- client/src/Layouts/Navbar.jsx
+++ client/src/Layouts/Navbar.jsx
@@ -23,38 +23,33 @@ const Navbar = () => {
 
   return (
     <>
-      {/*NAVBAR PARA ADMIN*/}
+      {/* NAVBAR */}
       {userInfo && userInfo.isAdmin ? (
         <nav className="bg-red-700 border-gray-200 dark:bg-gray-900">
-          {/*CONTENEDOR DE LOGO Y NOMBRE*/}
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <div>
-              <Link to="/" className="flex space-x-1 rtl:space-x-reverse">
-                <img
-                  src="/fukusuke-logo.png"
-                  className="w-24 h-24 rounded-full"
-                  alt="Fukusuke Logo"
-                />
-                <span className="self-center text-3xl font-extrabold whitespace-nowrap text-white dark:text-slate-600 rounded-lg px-4 py-2">
-                  Fukusuke Sushi
-                </span>
-              </Link>
-            </div>
-            <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            {/* Logo y Nombre */}
+            <Link to="/" className="flex items-center space-x-3">
+              <img
+                src="/fukusuke-logo.png"
+                className="w-16 h-16 sm:w-20 sm:h-20 rounded-full"
+                alt="Fukusuke Logo"
+              />
+              <span className="text-xl sm:text-3xl font-extrabold text-white whitespace-nowrap dark:text-slate-600 hidden sm:block">
+                Fukusuke Sushi
+              </span>
+            </Link>
+
+            {/* Botones de Usuario */}
+            <div className="flex space-x-3">
               {!userInfo ? (
                 <Link
                   to="/login"
-                  className="text-black bg-white hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-2 py-2 text-center border-4 border-black dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+                  className="text-black bg-white hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-4 py-2 text-center border-2 border-black dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Iniciar sesión →]
+                  Iniciar sesión →
                 </Link>
               ) : (
-                <>
-                  <UserDropdown
-                    logoutHandler={logoutHandler}
-                    isAdmin={true}
-                  ></UserDropdown>
-                </>
+                <UserDropdown logoutHandler={logoutHandler} isAdmin={true} />
               )}
             </div>
           </div>
@@ -62,41 +57,40 @@ const Navbar = () => {
       ) : (
         <nav className="bg-red-600 border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <Link
-              to="/"
-              className="flex items-center space-x-3 rtl:space-x-reverse"
-            >
-              {/* TODO!: AGREGAR LOGO */}
+            {/* Logo y Nombre */}
+            <Link to="/" className="flex items-center space-x-3">
               <img
                 src="/fukusuke-logo.png"
-                className="w-24 h-24 rounded-full"
+                className="w-16 h-16 sm:w-20 sm:h-20 rounded-full"
                 alt="Fukusuke Logo"
               />
-              <span className="self-center text-3xl font-extrabold whitespace-nowrap text-white dark:text-slate-600 rounded-lg px-4 py-2">
+              <span className="text-xl sm:text-3xl font-extrabold text-white whitespace-nowrap dark:text-slate-600 hidden sm:block">
                 Fukusuke Sushi
               </span>
             </Link>
-            <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+
+            {/* Opciones de Usuario */}
+            <div className="flex items-center space-x-3">
               {!userInfo ? (
                 <Link
                   to="/login"
-                  className="text-black bg-white hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-2 py-2 text-center border-2 border-black dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+                  className="text-black bg-white hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg text-sm px-4 py-2 text-center border-2 border-black dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Iniciar sesión →]
+                  Iniciar sesión →
                 </Link>
               ) : (
-                <>
-                  <UserDropdown
-                    logoutHandler={logoutHandler}
-                    isAdmin={false}
-                  ></UserDropdown>
+                <div className="flex items-center space-x-4 rtl:space-x-reverse">
+                  {/* MenuDropdown */}
+                  <MenuDropdown />
 
-                  <MenuDropdown></MenuDropdown>
+                  {/* UserDropdown */}
+                  <UserDropdown logoutHandler={logoutHandler} isAdmin={false} />
 
+                  {/* Button */}
                   <button
                     data-collapse-toggle="navbar-cta"
                     type="button"
-                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    className="inline-flex items-center p-2 justify-center text-sm text-custom-yellow rounded-lg hover:bg-gray-100 hover:text-yellow-600 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-cta"
                     aria-expanded="false"
                     onClick={() => setOpen(true)}
@@ -107,7 +101,7 @@ const Navbar = () => {
                       viewBox="0 0 24 24"
                       strokeWidth="1.5"
                       stroke="currentColor"
-                      className="size-6"
+                      className="w-6 h-6"
                     >
                       <path
                         strokeLinecap="round"
@@ -115,12 +109,12 @@ const Navbar = () => {
                         d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
                       />
                     </svg>
-
-                    <span>{qty}</span>
+                    <span className="ml-1 text-sm font-medium">{qty}</span>
                   </button>
 
-                  <Checkout open={open} setOpen={setOpen}></Checkout>
-                </>
+                  {/* Checkout */}
+                  <Checkout open={open} setOpen={setOpen} />
+                </div>
               )}
             </div>
           </div>
--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -7,7 +7,35 @@ export function UserDropdown({ logoutHandler, isAdmin }) {
   return (
     <>
       {isAdmin ? (
-        <Dropdown label="Admin" dismissOnClick={false} color="light">
+        <Dropdown
+          label="Admin"
+          dismissOnClick={false}
+          color="light"
+          renderTrigger={() => (
+            <button
+              type="button"
+              className="inline-flex items-center p-2 justify-center text-sm text-custom-yellow rounded-lg hover:bg-gray-100 hover:text-yellow-600 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="user-info"
+              aria-expanded="false"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zM12 14c-3.31 0-6 1.69-6 3v2h12v-2c0-1.31-2.69-3-6-3z"
+                />
+              </svg>
+              <span className="ml-1 text-sm font-medium">Perfil</span>
+            </button>
+          )}
+        >
           <Link to="/">
             <Dropdown.Item>Gestión de usuarios</Dropdown.Item>
           </Link>
@@ -17,7 +45,34 @@ export function UserDropdown({ logoutHandler, isAdmin }) {
           <Dropdown.Item onClick={logoutHandler}>Salir de admin</Dropdown.Item>
         </Dropdown>
       ) : (
-        <Dropdown label="Mi cuenta" dismissOnClick={false} color="light">
+        <Dropdown
+          label="Mi cuenta"
+          dismissOnClick={false}
+          color="light"
+          renderTrigger={() => (
+            <button
+              type="button"
+              className="inline-flex items-center p-2 justify-center text-sm text-custom-yellow rounded-lg hover:bg-gray-100 hover:text-yellow-600 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="user-info"
+              aria-expanded="false"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zM12 14c-3.31 0-6 1.69-6 3v2h12v-2c0-1.31-2.69-3-6-3z"
+                />
+              </svg>
+            </button>
+          )}
+        >
           <Link to="/order-history">
             <Dropdown.Item>Historial de pedidos</Dropdown.Item>
           </Link>
